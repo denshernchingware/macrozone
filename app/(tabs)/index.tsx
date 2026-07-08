5449f7568f39a258dc6d320955f58838bc5c3a14
denshernchingware <densher.n.chingware@gmail.com>
2026-07-08 19:13:08 +0530

    storage fnty

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,12 +1,33 @@
 import { globalStyles } from '../styles/globalStyles';
 import { Text, ScrollView } from 'react-native';
 import HomeHeader from '../components/HomeHeader';
+import MacroGrid from '../components/MacroGrid';
+import RecentMeals from '../components/RecentMeals';
+import { useFocusEffect } from 'expo-router';
+import { useCallback, useState } from 'react';
+import { getMeals, Meal } from '../storage/meals';
 
 export default function HomeScreen() {
+  const [meals, setMeals] = useState<Meal[]>([]);
+
+  const loadMeals = async () => {
+    const data = await getMeals();
+    setMeals(data);
+    console.log('Loaded meals:', data);
+  };
+
+  useFocusEffect(
+    useCallback(() => {
+      loadMeals();
+    }, []),
+  );
+
   return (
     <ScrollView style={globalStyles.container}>
       <Text style={globalStyles.title}>MacroZone</Text>
       <HomeHeader />
+      <MacroGrid />
+      <RecentMeals meals={meals} />
     </ScrollView>
   );
 }
